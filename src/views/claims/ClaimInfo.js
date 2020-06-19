c154ec0d9c26f20b45ec6bad645b1811340546a6
jackie-gdd <jxu@gooddeedsdata.com>
2020-06-19 08:15:17 -0400

    added approve/deny btns

--- src/views/claims/ClaimInfo.js
+++ src/views/claims/ClaimInfo.js
@@ -3,21 +3,37 @@ import PropTypes from 'prop-types';
 import Moment from 'react-moment';
 import { connect } from 'react-redux';
 import { getClaim } from '../../actions/claims';
+import { Button } from 'react-bootstrap';
 
-const ClaimInfo = ({ getClaim, claim: { claim }, match }) => {
+const ClaimInfo = ({ getClaim, claim: { claim }, match, history }) => {
   console.log(claim);
   useEffect(() => {
     getClaim(match.params.id);
   }, [getClaim, match.params.id]);
+
+  const popUp = () => {
+    if (window.confirm('Are you sure?')) {
+      /// Dispatch DELETE/APPROVE event here?
+
+      history.push('/claims');
+    }
+  };
+
   return claim === null ? (
     <div></div>
   ) : (
-    <div>
-      <Moment format="YYYY/MM/DD">{claim.date}</Moment>
-      {' | '}
-      {claim.description}
-      {' | '} {claim.user}
-    </div>
+    <Fragment>
+      <div>
+        <Moment format="YYYY/MM/DD">{claim.date}</Moment>
+        {' | '}
+        {claim.description}
+        {' | '} {claim.user}
+      </div>
+      <div>
+        <Button onClick={() => popUp()}>Approve</Button>{' '}
+        <Button onClick={() => popUp()}> Deny</Button>
+      </div>
+    </Fragment>
   );
 };
 

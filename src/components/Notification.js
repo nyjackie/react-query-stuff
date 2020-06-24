6bc9495f17fb945fc1112f697f607ac1f844d2f6
francisco-gdd <fgutierrez@gooddeedsdata.com>
2020-06-24 09:56:49 -0500

    added container

--- src/components/Notification.js
+++ src/components/Notification.js
@@ -4,34 +4,39 @@ import { connect } from 'react-redux';
 import { Toast } from 'react-bootstrap';
 import { setNotification } from 'actions/notifications';
 const Notification = ({ notifications, notifications: { id }, setNotification }) => {
+  if (!notifications || notifications.length === 0) {
+    return null;
+  }
+
   return (
-    notifications !== null &&
-    notifications.length > 0 &&
-    notifications.map(notification => {
-      return (
-        <div
-          aria-live="polite"
-          aria-atomic="true"
-          style={{ position: 'relative', minHeight: '45px' }}
-        >
-          <div style={{ position: 'absolute', top: 0, right: 0 }}>
-            <Toast
-              key={notification.id}
-              show={notification.show}
-              onClick={() => {
-                setNotification('', notification.id, false);
-              }}
-            >
-              <Toast.Header>
-                <strong className="mr-auto">{notification.msg}</strong>
-              </Toast.Header>
-            </Toast>
+    <div style={{ position: 'fixed', top: '20px', right: '20px', zIndex: '10' }}>
+      {notifications.map(notification => {
+        return (
+          <div
+            aria-live="polite"
+            aria-atomic="true"
+            style={{ position: 'relative', minHeight: '45px' }}
+          >
+            <div style={{ position: 'absolute', top: 0, right: 0 }}>
+              <Toast
+                key={notification.id}
+                show={notification.show}
+                onClick={() => {
+                  setNotification('', notification.id, false);
+                }}
+              >
+                <Toast.Header>
+                  <strong className="mr-auto">{notification.msg}</strong>
+                </Toast.Header>
+              </Toast>
+            </div>
           </div>
-        </div>
-      );
-    })
+        );
+      })}
+    </div>
   );
 };
+
 Notification.propTypes = {
   notifications: PropTypes.array.isRequired,
   setNotification: PropTypes.func.isRequired,

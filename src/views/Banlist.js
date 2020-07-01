462763f87f1a3bc95f2d3e9a58df8599f703b637
Francisco <fgutierrez@gooddeedsdata.com>
2020-07-01 10:11:12 -0500

    hooked up banned redux state to the view

--- src/views/Banlist.js
+++ src/views/Banlist.js
@@ -1,16 +1,58 @@
-import React, { Fragment } from 'react';
-import { Row, Col } from 'react-bootstrap';
+import React, { Fragment, useEffect } from 'react';
+import { connect } from 'react-redux';
+import { Row, Col, Table } from 'react-bootstrap';
+import { getBanned } from 'actions/banned';
 import PageHeader from 'components/PageHeader';
 
-function Banlist() {
+const BannedRow = ({ data }) => {
+  const { ein, name, bannedBy, bannedReason } = data;
+  return (
+    <tr>
+      <td>{ein}</td>
+      <td>{name}</td>
+      <td>{bannedBy}</td>
+      <td>{bannedReason}</td>
+    </tr>
+  );
+};
+
+function Banlist({ list, getBanned }) {
+  useEffect(() => {
+    getBanned().catch(err => {
+      console.error('error getting banned', err);
+    });
+  }, [getBanned]);
+
+  console.log(list);
+
   return (
     <Fragment>
       <PageHeader pageTitle="Banlist" />
       <Row>
-        <Col></Col>
+        <Col>
+          <Table striped bordered hover>
+            <thead>
+              <tr>
+                <th>ein</th>
+                <th>Name</th>
+                <th>Banned by</th>
+                <th>Ban reason</th>
+              </tr>
+            </thead>
+            <tbody>
+              {list.map(row => (
+                <BannedRow key={row.id} data={row} />
+              ))}
+            </tbody>
+          </Table>
+        </Col>
       </Row>
     </Fragment>
   );
 }
 
-export default Banlist;
+const mapStateToProps = state => ({
+  list: state.banned.list,
+});
+
+export default connect(mapStateToProps, { getBanned })(Banlist);

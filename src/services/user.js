2b3244742d33212a99044dfd42c6a1335817cc2d
Francisco <fgutierrez@gooddeedsdata.com>
2020-07-24 13:42:25 -0500

    updated loadUser to handle automatic refreshing if necessary

--- src/services/user.js
+++ src/services/user.js
@@ -1,7 +1,38 @@
 import jwt_decode from 'jwt-decode';
 import { api } from 'gdd-components';
+import { willExpire } from 'gdd-components/dist/api/helpers';
 import errorHandler from 'utils/errorHandler';
 import tokenStore from 'gdd-components/dist/api/tokenStore';
+import handleError from 'utils/errorHandler';
+
+/**
+ * replace the IndexedDB auth object with new data. If for some reason the accessToken
+ * is bad and does not decode properly then it will only store the refreshToken
+ * @param {string} accessToken
+ * @param {string} refreshToken
+ */
+async function updateLocalStore(accessToken, refreshToken) {
+  const authData = {
+    refreshToken,
+  };
+
+  let user;
+  try {
+    user = jwt_decode(accessToken);
+  } catch (e) {
+    // failed to decode, accessToken is bad
+  }
+
+  if (user && user.expires) {
+    // access token is good so we can store user and token
+    authData.user = user;
+    authData.accessToken = accessToken;
+  }
+
+  await tokenStore.set(authData);
+
+  return authData;
+}
 
 /**
  * Sends login info to api
@@ -14,15 +45,7 @@ export async function login(email, password) {
 
     api.setAuthHeader(res.data.accessToken);
 
-    const user = jwt_decode(res.data.accessToken);
-
-    const authData = {
-      user,
-      accessToken: res.data.accessToken,
-      refreshToken: res.data.refreshToken,
-    };
-
-    await tokenStore.set(authData);
+    const authData = await updateLocalStore(res.data.accessToken, res.data.refreshToken);
 
     return [null, authData];
   } catch (err) {
@@ -52,13 +75,39 @@ export async function resetPassword(email) {
 export async function loadUser() {
   try {
     const tokensData = await tokenStore.get();
-    if (tokensData) {
-      api.setAuthHeader(tokensData.accessToken);
+    let { user, accessToken, refreshToken } = tokensData || {};
+
+    if (!refreshToken) {
+      // if user has no refresh token for some reason we should just assume they are logged out
+      // because it's not worth letting them do anything for the rest of the 5min left in their
+      // access token because we cant refresh it
+      return false;
+    }
+
+    if (accessToken && !user?.expires) {
+      // if user does not exist but we have an access tokenn we can extract it
+      user = jwt_decode(accessToken);
+    }
+
+    if (!user) {
+      // if we can't decode the token and get user data then log them out
+      return false;
+    }
+
+    const newAccessToken = await api.handleRefresh(user.expires, refreshToken);
+    if (newAccessToken) {
+      // new token success, store it and continue
+      await updateLocalStore(newAccessToken, refreshToken);
+      api.setAuthHeader(newAccessToken);
+      tokensData.accessToken = newAccessToken;
       return tokensData;
     }
-    return false;
+
+    // local accessToken is still good
+    api.setAuthHeader(accessToken);
+    return tokensData;
   } catch (err) {
-    errorHandler(err);
+    errorHandler('Error occured on initial page load user', err);
     return false;
   }
 }

e8e830d44cbe280ac3e610d22d6a8d239b6b00bd
Francisco <fgutierrez@gooddeedsdata.com>
2020-07-24 13:40:53 -0500

    added some extra handling for api erros in errorHandler

--- src/utils/errorHandler.js
+++ src/utils/errorHandler.js
@@ -1,10 +1,26 @@
+function errorTypes(err) {
+  if (err.response) {
+    // this is an API error
+    return `API responded with: ${err.response.status}, for route: ${err.config.url}`;
+  }
+}
+
 /**
- * a global error handler just in case we want to start using some kind of third
- * party error tracking
- * @param {Error|string|object} err
+ * a global error handler
+ *
+ * TODO: this should eventually get a more defined interface and integrate with
+ * a third party like bugsnag or similar
  */
 export default function handleError() {
-  // for now it's just this but maybe in the future we might be sending
-  // data via an API or something.
-  console.error(...arguments);
+  [...arguments].forEach(arg => {
+    let msg = [];
+    if (arg instanceof Error) {
+      const specialErrors = errorTypes(arg);
+      if (specialErrors) {
+        msg.push(specialErrors);
+      }
+    }
+    msg.push(arg);
+    console.error(...msg);
+  });
 }

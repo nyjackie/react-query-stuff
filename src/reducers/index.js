0732d5df95b465b5f740d02dbebfb74820a6d7bc
Francisco <fgutierrez@gooddeedsdata.com>
2020-07-28 10:41:34 -0500

    removed unused reducers from combineReducers

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,18 +1,10 @@
 import { combineReducers } from 'redux';
 import auth from './auth';
 import ui from './ui';
-import loading from './loading';
 import notification from './notifications';
-import nonprofits from './nonprofits';
-import users from './users';
-import banned from './banned';
 
 export default combineReducers({
   notification,
   auth,
   ui,
-  loading,
-  nonprofits,
-  users,
-  banned,
 });

e4318d92b8b1a76d89a1a76212c086a33b4693cd
Francisco <francisco@givegooddeeds.com>
2020-09-23 11:58:23 -0400

    added unique checks to email and phone during admin user creation

--- src/hooks/useAdmin.js
+++ src/hooks/useAdmin.js
@@ -1,6 +1,13 @@
 import { useMutation } from 'react-query';
 import api from 'gdd-api-lib';
 
+export const USER_TYPES = {
+  CONSUMER: 'consumer',
+  NONPROFIT: 'nonprofit',
+  BRAND: 'brand',
+  INTERNAL: 'internal',
+};
+
 /****************************************************************
  * Functions that perform api calls
  */
@@ -9,10 +16,38 @@ function postUser(body) {
   return api.createInternalUser(body).then(res => res.data);
 }
 
+/**
+ * checks an email for unique per portal
+ * @param {object} body
+ * @param {string} body.email the user's email
+ * @param {string} body.user_type one of the USER_TYPES constants
+ */
+function uniqueEmail(body) {
+  return api.checkUniqueEmail(body).then(res => res.data);
+}
+
+/**
+ * checks a phone number for unique per portal
+ * @param {object} body
+ * @param {string} body.phone_number the user's email
+ * @param {string} body.user_type one of the USER_TYPES constants
+ */
+function uniquePhone(body) {
+  return api.checkUniquePhone(body).then(res => res.data);
+}
+
 /****************************************************************
  * Hooks
  */
 
 export function useCreateUser() {
   return useMutation(postUser);
+}
+
+export function useUniqueEmail() {
+  return useMutation(uniqueEmail);
+}
+
+export function useUniquePhone() {
+  return useMutation(uniquePhone);
 }

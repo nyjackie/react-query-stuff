3946d4f0c5ac8587331e9c914b6cb355c06b106d
Francisco <francisco@givegooddeeds.com>
2020-10-03 00:23:14 -0400

    added more props to image upload block and reduced css

--- src/components/ImageUploadBlock/index.js
+++ src/components/ImageUploadBlock/index.js
@@ -2,12 +2,22 @@ import React, { useState, useRef } from 'react';
 import { Button } from 'react-bootstrap';
 
 import { ImageUpload } from 'gdd-components';
-import { cn } from 'gdd-components/dist/utils';
 
 import Spinner from 'components/Spinner';
 import styles from './ImageUpload.module.scss';
 
-function ImageUploadBlock({ update_id, onSave, isLoading = false, reco, title, ...props }) {
+function ImageUploadBlock({
+  update_id,
+  onSave,
+  onImageSelected,
+  onError,
+  isLoading = false,
+  disabled,
+  reco,
+  title,
+  className,
+  ...props
+}) {
   const [validationError, setValidationError] = useState(null);
   const [src, setSrc] = useState(null);
 
@@ -24,7 +34,7 @@ function ImageUploadBlock({ update_id, onSave, isLoading = false, reco, title, .
       bytestring: src.replace('data:image/png;base64,', ''),
     })
       .then(() => {
-        // setCoverSrc(null);
+        setSrc(null);
       })
       .catch(err => {
         setSrc(null);
@@ -39,8 +49,9 @@ function ImageUploadBlock({ update_id, onSave, isLoading = false, reco, title, .
         </div>
       )}
 
-      <div className={styles.uploadBlock}>
-        <div className={cn(styles.uploadImg, styles.uploadImgCover)}>
+      <div className={className || null}>
+        <h3 className="h3">{title}</h3>
+        <div className="imgBlock-img">
           <ImageUpload
             {...props}
             ref={dropRef}
@@ -48,25 +59,28 @@ function ImageUploadBlock({ update_id, onSave, isLoading = false, reco, title, .
             onImageSelected={file => {
               setValidationError(null);
               setSrc(file.preview);
+              onImageSelected(file);
             }}
             onError={err => {
               setValidationError(err.message);
+              onError(err);
             }}
           />
         </div>
-        <div className={styles.uploadContent}>
-          <h3 className="h3">{title}</h3>
+        <div className="imgBlock-content">
           <p>{reco}</p>
-          <div className="d-flex justify-content-between">
-            <Button variant="primary" onClick={openDrop}>
-              Select image
-            </Button>
-            {src && (
-              <Button variant="success" onClick={saveCover}>
-                save
+          {!disabled && (
+            <div className="imgBlock-buttons">
+              <Button variant="primary" className="mr-4" onClick={openDrop}>
+                Select image
               </Button>
-            )}
-          </div>
+              {src && (
+                <Button variant="success" onClick={saveCover}>
+                  save
+                </Button>
+              )}
+            </div>
+          )}
         </div>
         {validationError && <p className={styles.uploadError}>{validationError}</p>}
       </div>

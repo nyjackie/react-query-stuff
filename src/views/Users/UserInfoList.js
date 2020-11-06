6d818d3ed9ab5b7b8ef2aad680cdd0a51998401a
Francisco <francisco@givegooddeeds.com>
2020-11-06 13:10:01 -0500

    removed redundant button and added no users message

--- src/views/Users/UserInfoList.js
+++ src/views/Users/UserInfoList.js
@@ -11,42 +11,38 @@ import UserInfo from 'views/Users/UserInfo';
 import Spinner from 'components/Spinner';
 
 function UserInfoList({ ids, type }) {
-  if (!ids.length) {
-    return null;
-  }
-
   return (
     <>
       <Accordion as={Container} className={styles.userEditList} defaultActiveKey={ids[0]}>
         <Card.Header>
-          <Col><h2>Users List</h2></Col>
+          <Col>
+            <h2>Users List</h2>
+          </Col>
         </Card.Header>
+        {!ids.length && (
+          <Card.Body>
+            <p>No users attached to this nonprofit</p>
+          </Card.Body>
+        )}
         {ids.map(id => {
           return (
             <Card key={id}>
               <Accordion.Toggle as={Card.Header} variant="link" eventKey={id}>
                 <Row>
                   <Col sm={8}>
-                    <Button variant={"link"}>
-                    User ID: {id}
-                    </Button>
-                  </Col>
-                  <Col sm={4}>
-                    <Button as={NavLink} style={{ float: 'right' }} variant={"link"} to={`/users/${type}/${id}`}>
-                      Go To User Page
-                    </Button>
+                    <Button variant={'link'}>User ID: {id}</Button>
                   </Col>
                 </Row>
               </Accordion.Toggle>
               <Accordion.Collapse eventKey={id}>
-                <UserInfo id={id} type={type} includeHeader={false}/>
+                <UserInfo id={id} type={type} includeHeader={false} />
               </Accordion.Collapse>
             </Card>
-          )
+          );
         })}
       </Accordion>
     </>
-  )
+  );
 }
 
 UserInfoList.propTypes = {

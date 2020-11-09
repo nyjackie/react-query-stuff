dcec94771f7a9d754a3a36fd93bbcd6114749c5e
Francisco <francisco@givegooddeeds.com>
2020-11-09 11:48:07 -0500

    updated no-console

--- src/utils/no-console.js
+++ src/utils/no-console.js
@@ -1,33 +1,7 @@
 if (process.env.NODE_ENV === 'production') {
   // borrowed from: https://github.com/h5bp/html5-boilerplate/blob/v8.0.0/src/js/plugins.js
   const noop = function () {};
-  const methods = [
-    'assert',
-    'clear',
-    'count',
-    'debug',
-    'dir',
-    'dirxml',
-    'error',
-    'exception',
-    'group',
-    'groupCollapsed',
-    'groupEnd',
-    'info',
-    'log',
-    'markTimeline',
-    'profile',
-    'profileEnd',
-    'table',
-    'time',
-    'timeEnd',
-    'timeline',
-    'timelineEnd',
-    'timeStamp',
-    'trace',
-    'warn',
-  ];
-  for (let method of methods) {
+  for (let method of Object.keys(window.console)) {
     window.console[method] = noop;
   }
 }

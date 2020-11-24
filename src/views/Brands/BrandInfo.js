8f875cfb94cf722a6c317689272110d7460839cf
Francisco <francisco@givegooddeeds.com>
2020-11-24 18:11:26 -0500

    updated the brand prop

--- src/views/Brands/BrandInfo.js
+++ src/views/Brands/BrandInfo.js
@@ -199,6 +199,8 @@ function BrandInfo({ addNotification, match }) {
                           brand={{
                             ...brand,
                             ...props.values,
+                            logo_url: brand.logo_url,
+                            hero_url: brand.hero_url,
                           }}
                         />
                       </Col>

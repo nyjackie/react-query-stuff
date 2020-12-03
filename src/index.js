446df9f17bebf88858a92a6d848747700b0516d9
Francisco <francisco@givegooddeeds.com>
2020-12-02 19:30:09 -0500

    removed web vitals because we dont need it for the admin portal

--- src/index.js
+++ src/index.js
@@ -7,7 +7,6 @@ import App from './App';
 import { Provider } from 'react-redux';
 import { ReactQueryConfigProvider } from 'react-query';
 import store from 'store';
-import reportWebVitals from './reportWebVitals';
 
 // kick off setting up our api
 import 'api/setup';
@@ -36,8 +35,3 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals(console.log);

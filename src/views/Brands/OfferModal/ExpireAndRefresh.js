282b7dddcdb08fdc0807eb2b3464614007290ee9
mattjaikaran-ggd <matt@givegooddeeds.com>
2020-12-18 12:29:14 -0500

    added handleConfirm with api call.

--- src/views/Brands/OfferModal/ExpireAndRefresh.js
+++ src/views/Brands/OfferModal/ExpireAndRefresh.js
@@ -1,15 +1,18 @@
 import React, { useState, Fragment, useEffect } from 'react';
+import axios from 'axios';
 import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
 import Toast from 'react-bootstrap/Toast';
 import Col from 'react-bootstrap/Col';
-
 
 function RefreshAndExpire() {
     const [showA, setShowA] = useState(false);
     const toggleShowA = () => setShowA(!showA);
     const [expireText, setExpireText] = useState('');
     const [btnDisabled, setBtnDisabled] = useState(true);
+    const [newOffer, setNewOffer] = useState(null)
+    const [error, setError] = useState([])
+    const url = 'https://endpointsportal.gdd-backend-asgard-staging.cloud.goog/docs/bifrost.endpoints.gdd-backend-asgard-staging.cloud.goog/0/routes/portal/internal/offer/refresh/post'
 
     useEffect(() => {
         if (expireText === 'I AM SURE') {
@@ -18,13 +21,26 @@ function RefreshAndExpire() {
         return () => setBtnDisabled(true)
     }, [expireText])
 
-    const handleChange = (e) => setExpireText(e.target.value)
+    const handleChange = e => setExpireText(e.target.value)
+
+    const handleConfirm = async e => {
+        try {
+            const response = await axios.post(url)
+            setNewOffer(response.data)
+        } catch (err) {
+            console.log(err)
+            setError(err)
+        }
+    }
     
     return (
         <Fragment>
             <Form.Row className="mt-3">
                 <Form.Group as={Col}>
                     <Button onClick={toggleShowA}>
+                        {/* <span style={{ color: 'white' }}>
+                            <i className="fa fa-sync" />
+                        </span> */}
                         Expire and Refresh Offer
                     </Button>
                     <Toast className="mt-3" show={showA} onClose={toggleShowA}>
@@ -36,6 +52,8 @@ function RefreshAndExpire() {
                             <p>
                                 Are you sure you want to expire this offer? 
                                 <br />
+                                You cannot undo this.
+                                <br />
                                 Type below {' '}
                                 <span className="text-danger">I AM SURE</span>
                             </p>
@@ -44,9 +62,12 @@ function RefreshAndExpire() {
                                 value={expireText}
                                 onChange={handleChange}
                             />
-                            <Button className="mt-3" disabled={btnDisabled} type="submit">Confirm</Button>
+                            <Button className="mt-3" disabled={btnDisabled} onClick={handleConfirm}>
+                                Confirm
+                            </Button>
                         </Toast.Body>
                     </Toast>
+                    <p>{newOffer || error.message}</p>
                 </Form.Group>
             </Form.Row>
         </Fragment>
